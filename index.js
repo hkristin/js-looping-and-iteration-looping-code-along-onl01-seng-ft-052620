597610bcd1dfe494c8b28932dda4c11ebec79cae
Kristin Howard <hkristin@andrew.cmu.edu>
2020-08-26 12:15:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 function writeCards(namesArray, event) {
   let thankYouCards = []
-  for(let i = 0; i < names.length; i++) {
+  for(let i = 0; i < namesArray.length; i++) {
     thankYouCards.push('Thank you, ${namesArray[i]}, for the wonderful ${event} gift!');
   }
   return thankYouCards
