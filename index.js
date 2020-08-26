f4c56a2215ee4cc6386532161ee15c60e37304fe
Kristin Howard <hkristin@andrew.cmu.edu>
2020-08-26 12:17:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,3 +6,11 @@ function writeCards(names, event) {
   }
   return thankYouCards
 }
+
+function countDown( startingNumber ) {
+  while ( startingNumber > 0 ) {
+    console.log( startingNumber );
+    startingNumber -= 1;
+  }
+  console.log( startingNumber );
+}
